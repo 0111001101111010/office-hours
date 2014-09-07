62f6e8a33a400c254d0780373989b50d043c7a61
stanley zheng <dev.stanzheng@gmail.com>
2014-09-07 04:06:31 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var Twitter = require('node-tweet-stream');
+var t = new Twitter('./config.json');
+t.on('tweet', function (tweet) {
+  //console.log(tweet);
+  /*
+  Not RT
+  In user.lang='en'
+  */
+  if(!tweet.retweeted && tweet.user.lang==='en'){
+console.log(tweet.text);
+  }
+});
+
+t.on('error', function (err) {
+  console.log('Oh no');
+});
+
+ // t.track(' ');
+ t.track('android');
+//
+// // 5 minutes later
+// t.track('tacos');
+//
+// // 10 minutes later
+// t.untrack('pizza');
